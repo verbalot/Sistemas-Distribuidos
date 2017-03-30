b3e8561a1dbc9cd11f2028fdad1ff850789ee3c1
verbalot <jesusbanur@gmail.com>
2017-03-30 20:49:20 +0200

    Add files via upload

--- Biblioteca01/src/main/java/com/example/TablonController.java
+++ Biblioteca01/src/main/java/com/example/TablonController.java
@@ -1,80 +1,54 @@
 package com.example;
 
-import java.util.ArrayList;
-import java.util.List;
+import javax.annotation.PostConstruct;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class TablonController {
-	private List<Libro> catalogo = new ArrayList<>();
 	
-	private List<Usuario> usuarios = new ArrayList<>();
-	
-	public TablonController() {
-		
-		
-		catalogo.add(new Libro("Capitan Alatriste", "Perez Reverte, Arturo","Novela Historica"));
-		catalogo.add(new Libro("Episodios Nacionales", "Perez Galdos, Benito","Novela Historica"));
-		catalogo.add(new Libro("Calculus","Escritor","Ciencia Aplicada"));
-		
-		usuarios.add(new Usuario("admin","admin"));
-		usuarios.add(new Usuario("pedro","guerra"));
-		usuarios.add(new Usuario("maria","fernandez"));
+	//Si se abre la URL http://127.0.0.1:8080/h2-console y se configura
+	//la URL JDBC con el valor jdbc:h2:mem:testdb se puede acceder a la 
+	//base de datos de la aplicación
+
+	@Autowired
+	private BibliotecaRepository repository;
+
+	@PostConstruct
+	public void init() {
+		//repository.save(new Anuncio("Jesus", "Banegas", "jesusbanur@gmail.com","676155144"));
+		repository.save(new Biblioteca());
 	}
 
-	
+	@RequestMapping("/")
+	public String tablon(Model model, Pageable page) {
 
-	
-	@GetMapping("/presentacion")
-	public String presentacion(Model model) {
-
-		model.addAttribute("catalogo", catalogo);
+		model.addAttribute("biblioteca", repository.findAll(page));
 
 		return "presentacion";
 	}
 
-	
-	@GetMapping("/catalogo")
-	public String catalogo(Model model) {
+	@RequestMapping("/usuario/nuevo")
+	public String nuevoAnuncio(Model model, Biblioteca biblioteca) {
 
-		model.addAttribute("catalogo", catalogo);
-
-		return "catalogo";
-	}
-	
-	
-	@PostMapping("/libro/nuevo")
-	public String nuevoLibro(Model model, Libro libro) {
-
-		catalogo.add(libro);
-
-		return "libro_guardado";
-
-	}
-	
-
-	@GetMapping("/ver_libro/{num}")
-	public String libro(Model model, @PathVariable int num) {
-
-		Libro libro = catalogo.get(num - 1);
-
-		model.addAttribute("libro", libro);
-
-		return "ver_libro";
-	}
-	
-	@PostMapping("/usuario/nuevo")
-	public String nuevoUsuario(Model model, Usuario usuario) {
-
-		usuarios.add(usuario);
+		repository.save(biblioteca);
 
 		return "usuario_guardado";
 
 	}
 
+	@RequestMapping("/anuncio/{id}")
+	public String verAnuncio(Model model, @PathVariable long id) {
+		
+		Biblioteca biblioteca = repository.findOne(id);
+
+		model.addAttribute("anuncio", biblioteca);
+
+		return "ver_anuncio";
+	}
 }
--- Biblioteca01/src/main/java/com/example/Usuario.java
+++ Biblioteca01/src/main/java/com/example/Usuario.java
@@ -1,6 +1,15 @@
 package com.example;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Usuario {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long id;
 
 	private String login;
 	private String password;

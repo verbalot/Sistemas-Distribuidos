e33cd5a9d69abdf0f100295cb7629d2a10d3faf7
verbalot <jesusbanur@gmail.com>
2017-04-01 17:11:57 +0200

    Add files via upload

---
+++ Biblioteca01/src/main/java/com/example/TablonController.java
@@ -0,0 +1,26 @@
+package com.example;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class TablonController {
+	
+	//Si se abre la URL http://127.0.0.1:8080/h2-console y se configura
+	//la URL JDBC con el valor jdbc:h2:mem:testdb se puede acceder a la 
+	//base de datos de la aplicación
+
+	
+	
+	@Autowired
+	private AccesosRepository repository;
+	
+	
+
+}

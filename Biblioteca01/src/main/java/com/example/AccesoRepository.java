762e71c38c84f4a9bcac55d4200992467ca1c50f
verbalot <jesusbanur@gmail.com>
2017-04-13 21:11:51 +0200

    Update AccesoRepository.java

--- Biblioteca01/src/main/java/com/example/AccesoRepository.java
+++ Biblioteca01/src/main/java/com/example/AccesoRepository.java
@@ -1,7 +1,12 @@
 package com.example;
 
+import com.example.*;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AccesoRepository extends JpaRepository<Acceso, Long> {
+	
+	Acceso findByLogin(String login);
+	
+	Acceso findByPassword(String password);
 
 }

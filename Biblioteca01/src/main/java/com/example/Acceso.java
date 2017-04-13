9953cb3e626f9ddb90f2e5a315febd479e9b6d13
verbalot <jesusbanur@gmail.com>
2017-04-13 19:58:12 +0200

    Update Acceso.java

--- Biblioteca01/src/main/java/com/example/Acceso.java
+++ Biblioteca01/src/main/java/com/example/Acceso.java
@@ -6,6 +6,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import java.util.List;
 
 @Entity
 public class Acceso {
